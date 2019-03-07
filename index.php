aeed4c26e06bcfde309d161253e26ab7507dfcc1
Fabian Saldaño <cetecom@LCT50415>
2019-03-06 22:14:03 -0300

    actualización

--- index.php
+++ index.php
@@ -18,6 +18,28 @@ and open the template in the editor.
             echo "Hola".$saludo;        
         ?>
         
+        <?php
+        
+        $meses["enero"]="verano";    
+        $meses["febrero"]="verano";        
+        $meses["marzo"]="verano/otoño";        
+        $meses["abril"]="otoño";        
+        $meses["mayo"]="otoño/invierno";        
+        $meses["junio"]="invierno";        
+        $meses["julio"]="invierno";        
+        $meses["agosto"]="invierno";             
+        $meses["septiembre"]="invierno/primavera";             
+        $meses["osctubre"]="primavera";             
+        $meses["noviembre"]="primavera";             
+        $meses["diciembre"]="primavera/verano";             
+                
+        
+        foreach ($meses as $mes){
+            echo $mes."<br>";
+        }            
+           
+        ?>
+        
         <br>
         Variables Bloblares $_SERVER
         <br>
